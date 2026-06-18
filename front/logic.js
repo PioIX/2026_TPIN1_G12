0676f84da6869ebb21e817d529fe1752839ae96c
lcontrera143 <lcontrera@pioix.edu.ar>
2026-06-18 10:06:50 -0300

    organización nomás

---
+++ front/logic.js
@@ -0,0 +1,23 @@
+//     esta es mi parte :P LINO
+
+/* // INFO PARA Botones Cambiar de página
+// Seleccionamos el botón por su ID
+const boton = document.getElementById("miBoton");
+
+// Escuchamos el evento 'click'
+boton.addEventListener("click", function() {
+    // Cambiamos el archivo al que queremos redirigir
+    window.location.href = "pagina2.html";
+});
+*/
+
+
+// FUNCIONES A HACER:
+/*
+
+pantalla 1:
+ - verificar inicio de sesión
+ - o verificar registro
+
+
+*/
