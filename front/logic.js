e5cd0919f1d77c62ac7fdccfa38af521d1401f2b
lcontrera143 <lcontrera@pioix.edu.ar>
2026-06-25 22:06:59 -0300

    login y registro yaya

--- front/logic.js
+++ front/logic.js
@@ -22,10 +22,6 @@ let id_user = -1
 
 // LOGIN --------------------------------------------------------------------------------------
 
-//tengo q comprobar si el usuario y su contraseña están bien, y devolver el id_user x las dudas, y si anda mal, alert
-// usar app.get de todos los usuarios para comparar uno a uno
-
-// se supone que tiene que checar si ya existe el usuario
 async function inicioSesion() {
     let usu = document.getElementById("usuario").value;
     console.log(usu)
@@ -65,7 +61,7 @@ async function inicioSesion() {
         }
 
         if (usuarioCheck) {
-            window.location.href = "pantalla0.html";
+            window.location.href = "menu.html";
         } else {
             alert("Usuario o contraseña incorrectos")
         }
@@ -74,79 +70,6 @@ async function inicioSesion() {
 }
 
 //  REGISTROO --------------------------------------------------------------------------------------
-
-//usar app.post de usernuevo
-// dudas sobre cómo incluir si es admin o no
-
-async function Registrarse() {
-    let usu = getUsuario();
-    console.log(usu)
-
-    let contra = getContra();
-    console.log(contra)
-
-    let nom = getNombre();
-    console.log(nom)
-
-    let response = await fetch('http://localhost:4000/todousuarios', {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    })
-
-    let res = await response.json()
-    let usuExiste = false
-
-
-    if (usu == "" || contra == "" || nom == "") {
-        alert("Todos los campos deben estar completos")
-    } else {
-        for (let i = 0; i < res.length; i++) {
-            if (res[i].usuario == usu) {
-                console.log("usuario existe")
-                console.log(res[i].usuario)
-                usuExiste = true
-            }
-        }
-
-        if (usuExiste) {
-            alert("Este nombre de usuario ya existe, intente con otro")
-        } else {
-
-            // no recuerdo como mandarle los datos al post :P
-            // supuestamente puede ser así:
-            /*
-            let data = {
-
-                nombre_completo: nombre,
-                usuario: usuario,
-                contraseña: contra,
-
-            }
-
-            await fetch(
-                "http://localhost:4000/usernuevo",
-                {
-
-                    method: "POST",
-
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-
-                    body: JSON.stringify(data)
-
-                }
-            )
-            */
-
-            window.location.href = "pantalla0.html";
-        }
-    }
-
-
-}
 
 async function usuPost(datos) {
     const resultado = await fetch("http://localhost:4000/usernuevo", {
@@ -164,20 +87,20 @@ async function usuPost(datos) {
 
 function Registrarse() {
     datos = {
-        nombre: getNombre(),
-        rubro: getRubro(),
-        cant_empleados: getCant_empleados(),
-        id_pais: getId_pais()
+        usu: getUsuario(),
+        contra: getContra(),
+        nom: getNombre(),
+        es_admin: 0
     }
 
     let usuExiste = false
 
-
-    if (usu == "" || contra == "" || nom == "") {
+    if (datos.usu == "" || datos.contra == "" || datos.nom == "") {
         alert("Todos los campos deben estar completos")
+        return
     } else {
         for (let i = 0; i < res.length; i++) {
-            if (res[i].usuario == usu) {
+            if (res[i].usuario == datos.usu) {
                 console.log("usuario existe")
                 console.log(res[i].usuario)
                 usuExiste = true
@@ -187,13 +110,11 @@ function Registrarse() {
         if (usuExiste) {
             alert("Este nombre de usuario ya existe, intente con otro")
         } else {
-
-
-            usuPost(datos)
+            await usuPost(datos)
+            window.location.href = "menu.html";
         }
     }
 }
-
 
 
 
